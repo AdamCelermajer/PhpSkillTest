a0d8ae462dcf8f297bdb1e61470c27dffe4ab42f
AdamCelermajer <39280082+AdamCelermajer@users.noreply.github.com>
2018-07-27 14:58:50 +0100

    Beta
    decrypt beta

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <b>Wellcome to this small PHP skill test.</b>
-        
         <?php
         
+        class decrypting{
+           
+            
+            /* if($index!=0){
+                base64_decode($data);
+                return decrypt($index-1,$data);
+
+                }
+                return $data;
+            }
+            */
+           
+            function decrypt($index,$data){
+          
+                while($index!=0){
+              
+                    $data=base64_decode($data);
+                    
+                }
+                return $data;
+            }
+        }
+        
+        
         ?>
+       
     </body>
 </html>
