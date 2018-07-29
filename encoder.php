22d4d063aeb5ff5b22442c4142b44295901e6615
ACelermajer <acelermajer@ashford.com>
2018-07-29 13:00:45 +0300

    Ready Player One

--- encoder.php
+++ encoder.php
@@ -13,7 +13,9 @@ spl_autoload_register(function ($class_name) {
 function proceedEncoding() {
     $C = "encoder_" . BaseConvert(rand(0, 1000), "0123456789", "QWERTYUIOPASDFGHJKLZXCVBNM");
     // Tip the folder contening the encoder_XXX is equal to round((XXX/50)+5000) 
+    
     $c = new $C();
+    
     echo $c->encode("Text to Encode:".$_POST["txt"]);
 }
 
