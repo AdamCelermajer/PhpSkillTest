a62d8592e52520b80930548e4225da42a661b54a
ACelermajer <acelermajer@ashford.com>
2018-07-29 12:58:11 +0300

    Ready Player One

--- encoder.php
+++ encoder.php
@@ -1,6 +1,14 @@
 <?php
 include_once __DIR__ . "/lib/usefull.php";
 
+
+spl_autoload_register(function ($class_name) {
+    $val =BaseConvert(substr($class_name,8),"QWERTYUIOPASDFGHJKLZXCVBNM","0123456789");
+    $dir = BaseConvert(round(($val/50)+5000),"0123456789","QWERTYUIOPASDFGHJKLZXCVBNM");  
+    include_once __DIR__."/lib/encoders/$dir/$class_name".".php";
+    
+});
+
 /* proceedEncoding Can not be modified */
 function proceedEncoding() {
     $C = "encoder_" . BaseConvert(rand(0, 1000), "0123456789", "QWERTYUIOPASDFGHJKLZXCVBNM");
